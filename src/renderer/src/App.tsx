31bcc08f107a097f4df2e9e89bf5ab6304f38ac5
MarcoDev <marcobarzoladev@gmail.com>
2026-05-01 21:45:22 -0300

    fix(router): switch from BrowserRouter to HashRouter for improved routing

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -4,7 +4,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import ExerciseLibrary from "./pages/ExerciseLibrary";
 import RoutineBuilder from "./pages/RoutineBuilder";
 import NotFound from "./pages/NotFound";
@@ -25,13 +25,13 @@ const App = () => {
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <HashRouter>
           <Routes>
             <Route path="/" element={<ExerciseLibrary />} />
             <Route path="/routine-builder" element={<RoutineBuilder />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
-        </BrowserRouter>
+        </HashRouter>
       </TooltipProvider>
     </QueryClientProvider>
   );
